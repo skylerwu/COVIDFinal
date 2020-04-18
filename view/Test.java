28ede4227a00906456c88f86aa176d3347446e2c
Allen Yang <allen.rao.yang@gmail.com>
2020-04-17 20:27:04 -0700

    adds jars, converts to maven project

---
+++ view/Test.java
@@ -0,0 +1,16 @@
+package view;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+public class Test {
+}
