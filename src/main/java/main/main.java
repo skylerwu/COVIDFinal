6b0080e420971fd44d280911b5bb26472ce00211
Allen Yang <allen.rao.yang@gmail.com>
2020-04-22 08:13:21 -0700

    test

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -2,6 +2,13 @@ package main;
 
 import java.util.Arrays;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -40,7 +47,7 @@ public class main {
             throw new AmazonClientException(
                     "Cannot load the credentials from the credential profiles file. " +
                     "Please make sure that your credentials file is at the correct " +
-                    "location (/Users/johnmortensen/.aws/credentials), and is in valid format.",
+                    "location (/Users/AllenYang/.aws/credentials), and is in valid format.",
                     e);
         }
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
@@ -64,7 +71,47 @@ public class main {
             //System.err.println("Unable to read item: " + year + " " + title);
             System.err.println(e.getMessage());
         }
-
         
+        UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("State", "California", "County", "San Diego")
+                .withUpdateExpression("set info.rating = :r, info.plot=:p, info.actors=:a")
+                .withValueMap(new ValueMap().withNumber(":r", 5.5).withString(":p", "Everything happens all at once.")
+                    .withList(":a", Arrays.asList("Larry", "Moe", "Curly")))
+                .withReturnValues(ReturnValue.UPDATED_NEW);
+        try {
+            System.out.println("Updating the item...");
+            UpdateItemOutcome outcome = data.updateItem(updateItemSpec);
+            System.out.println("UpdateItem succeeded:\n" + outcome.getItem().toJSONPretty());
+
+        }
+        catch (Exception e) {
+            System.err.println("Unable to update item: ");
+            System.err.println(e.getMessage());
+        }
+        
+     // Make a URL to the web page
+        URL url = new URL("https://www.kff.org/health-costs/issue-brief/state-data-and-policy-actions-to-address-coronavirus/#casesdeathstrend");
+
+        // Get the input stream through URL Connection
+        URLConnection con = url.openConnection();
+        InputStream is =con.getInputStream();
+
+        // Once you have the Input Stream, it's just plain old Java IO stuff.
+
+        // For this case, since you are interested in getting plain-text web page
+        // I'll use a reader and output the text content to System.out.
+
+        // For binary content, it's better to directly read the bytes from stream and write
+        // to the target file.
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+
+        // read each line and write to System.out
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
     }
+        
 }
